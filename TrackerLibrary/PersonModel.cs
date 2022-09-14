c17850fba569ceca08f5c8cfb81eed7ea166168f
Bartiszny <29127947+Bartiszny@users.noreply.github.com>
2022-09-14 23:25:41 +0200

    Add the remaining model library files

--- TrackerLibrary/PersonModel.cs
+++ TrackerLibrary/PersonModel.cs
@@ -6,12 +6,11 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
-    public class TournamentModel
+    public class PersonModel
     {
-        public string TournamentName { get; set; }
-        public decimal EntryFee { get; set; }
-        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
-        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
-        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string CellphoneNumber { get; set; }
     }
 }
